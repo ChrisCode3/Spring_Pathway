ab99a4b00b3843e8df299ff9fb17f507b047a32d
ChrisCode3 <malusdarkblade66@gmail.com>
2026-08-14 13:24:02 +0100

    created new package for Accessing Data with JPA

---
+++ Accessing_Data_with_JPA/src/main/java/Accessing_Data_with_JPA/AccessingDataWithJpaApplication.java
@@ -0,0 +1,13 @@
+package Accessing_Data_with_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataWithJpaApplication.class, args);
+	}
+
+}
---
+++ Accessing_Data_with_JPA/src/test/java/Accessing_Data_with_JPA/AccessingDataWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package Accessing_Data_with_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
